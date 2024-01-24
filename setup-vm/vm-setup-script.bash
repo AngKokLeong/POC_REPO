c99d119fb08b78a38241208b17d6b5045f729b68
AngKokLeong <ang.kok.leong.development@gmail.com>
2024-01-24 03:21:54 +0000

    added skeleton structure for the script setting up the VM

---
+++ setup-vm/vm-setup-script.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+apt-get update -yq
+
+#Install packages
+
+
+#Setup Docker Packages
+
+
+
+#Setup Netplan to customize IP Address
+
+
+#Setup hostname
+
+
+
+#Setup hosts
+
+
+
+#Setup Puppet
+
+
+
+#Setup Nagios in VM
+
+
+
