8797f0d8ae9ee347341b4e4c056496b6693d4861
AngKokLeong <ang.kok.leong.development@gmail.com>
2024-01-25 07:39:22 +0000

    modified the order of execution for netplan configuration

--- setup-vm/vm-setup-script.bash
+++ setup-vm/vm-setup-script.bash
@@ -3,16 +3,12 @@
 apt-get update -yq
 
 #Install packages
-apt-get install net-tools autoconf gcc libc6 make wget unzip apache2 php libapache2-mod-php7.4 libgd-dev -yq
+apt-get install net-tools  -yq
 
 #Setup Docker Packages
 /bin/bash docker-installation-script.bash -v 
 
-#Setup Netplan to customize IP Address
-cat netplan.yaml > /etc/netplan/01-network-manager-all.yaml
 
-# Apply the netplan
-netplan apply   
 
 #Setup hostname
 echo ''
@@ -22,12 +18,11 @@ cat hostname > /etc/hostname
 echo ''
 cat hosts > /etc/hosts
 
-#Setup Puppet
-/bin/bash puppet-setup-script.bash -v
+#Setup Netplan to customize IP Address
+cat netplan.yaml > /etc/netplan/01-network-manager-all.yaml
+
+# Apply the netplan
+netplan apply   
 
 
-#Setup Nagios in VM
 
-
-
-#Setup SSH access of docker container related to Nagios
