cbf9ab22df214fb4e47d08a4e5b3a82282e4e749
AngKokLeong <ang.kok.leong.development@gmail.com>
2024-01-29 14:32:42 +0000

    added script for setting up nagios in the vm

---
+++ setup-vm/nagios-setup-script.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cd /home/dockeradm/Desktop
+
+apt-get update -yq
+apt-get autoconf gcc libc6 make wget unzip apache2 php libapache2-mod-php7.2 libgd-dev -yq
+
+#Modify apache2 server port number
+
+
+
+
+
+# Add new user nagios
+useradd nagios -mU
+usermod -a -G nagios www-data
+
+
+
+#Install Nagios
+
+#Retrieve from this link 'https://www.nagios.org/downloads/nagios-core/thanks/'
+wget --inet4-only --content-disposition 'https://go.nagios.org/get-core/4-5-0/' -O nagios-4.5.0.tar.gz
+
+tar -xzvf nagios-4.5.0.tar.gz
+rm nagios-4.5.0.tar.gz
+
+cd nagios-4.5.0
+
+./configure --with-httpd-conf=/etc/apache2/sites-enabled
+
+make all
+
+
+#Setup Nagios Plugin
+cd /home/dockeradm/Desktop
+
+wget --inet4-only --content-disposition 'https://github.com/nagios-plugins/nagios-plugins/archive/refs/tags/2.4.8.tar.gz' -O nagios-plugins-2.4.8.tar.gz
+
+tar -xzvf nagios-plugins-2.4.8.tar.gz
+rm nagios-plugins-2.4.8.tar.gz
+
