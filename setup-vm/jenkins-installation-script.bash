dada2415934738cc5ffdae8001930b5fca9e92f6
AngKokLeong <ang.kok.leong.development@gmail.com>
2024-02-01 08:20:30 +0000

    added jenkins installation script

---
+++ setup-vm/jenkins-installation-script.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update -yq
+sudo apt install fontconfig openjdk-17-jre -yq
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -yq
+sudo apt-get install jenkins -yq
+
+
