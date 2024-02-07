6cddd21b22b7add24e44b44255f4784d6ccc20b0
AngKokLeong <ang.kok.leong.development@gmail.com>
2024-02-07 10:01:28 +0000

    modified the puppet installation file to 2023.6.0

--- setup-vm/puppet-setup-script.bash
+++ setup-vm/puppet-setup-script.bash
@@ -6,25 +6,28 @@
 #5.1 Download the package using wget 
 # Refer to https://www.ubuntumint.com/wget-force-ipv4-ipv6-connection/
 # Puppet download link retrieved from https://www.puppet.com/downloads/puppet-enterprise 
-wget --content-disposition --inet4-only 'https://pm.puppet.com/cgi-bin/download.cgi?dist=ubuntu&rel=18.04&arch=amd64&ver=latest' 
+# # reference from https://www.puppet.com/releases/202360 for 2023.6.0
+wget --content-disposition --inet4-only 'https://pm.puppet.com/cgi-bin/download.cgi?dist=ubuntu&rel=18.04&arch=amd64&ver=2023.6.0' 
+
+
 
 #5.2 Unpack the archive
 echo 'Step 4.2 Unpack the archive'
 
 # using gunzip
 echo 'Extract the archive using gunzip'
-gunzip puppet-enterprise-2023.5.0-ubuntu-18.04-amd64.tar.gz
+gunzip puppet-enterprise-2023.6.0-ubuntu-18.04-amd64.tar.gz
 
 #then tar xvf
 echo 'Extracting the files in tar archive'
-tar xvf puppet-enterprise-2023.5.0-ubuntu-18.04-amd64.tar
+tar xvf puppet-enterprise-2023.6.0-ubuntu-18.04-amd64.tar
 
 
 # Remove the tar archive file
-rm puppet-enterprise-2023.5.0-ubuntu-18.04-amd64.tar
+rm puppet-enterprise-2023.6.0-ubuntu-18.04-amd64.tar
 
 echo 'Change working directory to puppet folder'
-cd puppet-enterprise-2023.5.0-ubuntu-18.04-amd64
+cd puppet-enterprise-2023.6.0-ubuntu-18.04-amd64
 
 #5.3 Run the installer
 echo 'Running the installer for Puppet Enterprise'
